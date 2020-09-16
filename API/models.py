9a98ce0c0224d4e65fc9f81a0ed8e7bdc3cdbec9
konstrictor <jonkur96@gmail.com>
2020-09-17 01:01:08 +0200

    move ticket from payment to profile

--- API/models.py
+++ API/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Profile(models.Model):
-	user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
-	avatar = models.ImageField(upload_to="profiles/")
-	phone = models.CharField(max_length=16, null=False, blank=False)
-	mobile = models.CharField(max_length=16, blank=True, null=True)
-	date = models.DateField(default=timezone.now)
-	autres = models.TextField(blank=True, null=True)
-
 class Ticket(models.Model):
 	name = models.CharField(max_length=20)
 	somme = models.PositiveIntegerField(null=False, blank=False)
 	consommable = models.PositiveIntegerField(default=0)
 	autres = models.TextField(blank=True, null=True)
 
+class Profile(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+	avatar = models.ImageField(upload_to="profiles/")
+	phone = models.CharField(max_length=16, null=False, blank=False)
+	mobile = models.CharField(max_length=16, blank=True, null=True)
+	date = models.DateField(default=timezone.now)
+	ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+	autres = models.TextField(blank=True, null=True)
+
 class Payment(models.Model):
 	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
 	date = models.DateField(default=timezone.now)
 	autres = models.TextField(blank=True, null=True)
 
