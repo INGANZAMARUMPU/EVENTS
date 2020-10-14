81111218230c15d70e35bf9592e1fcac07df7b4c
konstrictor <jonkur96@gmail.com>
2020-10-14 11:25:14 +0200

    solved printall ticket issues

--- APP/static/js/main.js
+++ APP/static/js/main.js
@@ -6,78 +6,25 @@ function closest(el, selector, stopSelector) {
   if (el.matches(stopSelector)) return null;
   return closest(el.parentNode, selector, stopSelector);
 }
-
-function templateTicket(infos){
-	var qr = QRCode.generatePNG(infos.qr_text, {
-	    ecclevel : 'L'
-	});
+function templateHead(){
 	return`
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<style type="text/css" media="screen">
-		*{
-			padding: 0;
-			margin: 0;
-		}
-		.card{
-			position: relative;
-			background: black;
-			width: 10cm;
-			height: 5cm;
-			color: white;
-		}
-		.cadre1{
-			position: absolute;
-			border-left: 2px solid white;
-			border-bottom: 2px solid white;
-			width: 2.5cm;
-			height: 2.5cm;
-			top: 1.75cm;
-			left: 1cm;
-		}
-		.cadre2{
-			position: absolute;
-			border-top: 2px solid white;
-			border-right: 2px solid white;
-			width: 5cm;
-			height: 3.25cm;
-			top:1cm;
-			left: 3.75cm;
-		}
-		.logo img{
-			width: 2.5cm;
-		}
-		.logo{
-			position: absolute;
-			width: 2.5cm;
-			left: 1.5cm;
-			top: 1.25cm;
-		}
-		.alpha{
-			position: absolute;
-			left:1.25cm;
-			font-size: 2em;
-			top: 0.25cm;
-		}
-		.title{
-			position: absolute;
-			left: 4.5cm;
-			top: 0.5cm;
-			font-weight: bold;
-		}
-		.qr img{
-			width: 2.5cm;
-		}
-		.qr{
-			position: absolute;
-			bottom: 0.7cm;
-			right: 1.24cm;
-		}
+	<style type="text/css">
+*{padding:0;margin:0}.container{display: inline-block;}.card{position:relative;background:#000;width:10cm;height:5cm;color:#fff;margin:2px;}.cadre1{position:absolute;border-left:2px solid #fff;border-bottom:2px solid #fff;width:2.5cm;height:2.5cm;top:1.75cm;left:1cm}.cadre2{position:absolute;border-top:2px solid #fff;border-right:2px solid #fff;width:5cm;height:3.25cm;top:1cm;left:3.75cm}.logo img{width:2.5cm}.logo{position:absolute;width:2.5cm;left:1.5cm;top:1.25cm}.alpha{position:absolute;left:1.25cm;font-size:2em;top:.25cm}.title{position:absolute;left:4.5cm;top:.5cm;font-weight:700}.qr img{width:2.5cm}.qr{position:absolute;bottom:.7cm;right:1.24cm}
 	</style>
 </head>
 <body>
+	`
+}
+function templateBody(infos){
+	var qr = QRCode.generatePNG(infos.qr_text, {
+	    ecclevel : 'L'
+	});
+	return`
 	<div class="container">
 		<div class="card">
 			<div class="alpha">Alpha</div>
@@ -88,10 +35,11 @@ function templateTicket(infos){
 			<div class="qr"><img src="${qr}" alt=""/></div>
 		</div>
 	</div>
-</body>
 		`
 }
-
+function templateFoot(){
+	return`</body></head></html>`
+}
 function generateTicket(event){
 	user_card = closest(event.target, '.user-card', '.component');
 
@@ -116,11 +64,11 @@ function generateTicket(event){
 		logo: document.getElementById('logo').getAttribute('src'),
 		qr_text: user_card.getAttribute('qr-data'),
 	}
-	printTicket(templateTicket(infos));
+	printTicket(templateBody(infos));
 }
 function generateTickets(){
 	var user_cards = document.querySelectorAll(".user-card");
-	div = `<div style="display: inline-flex;">`;
+	div = "";
 	user_cards.forEach(function(user_card){
 		var userSelector = function(str){
 			return user_card.querySelector(str); 
@@ -140,21 +88,23 @@ function generateTickets(){
 			event_date: userSelector('.date').textContent,
 			event_place: userSelector('.place').textContent,
 			ticket_date: userSelector('.ticket-date').textContent,
+			logo: document.getElementById('logo').getAttribute('src'),
 			qr_text: user_card.getAttribute('qr-data'),
 		}
-		div += templateTicket(infos);
+		div += templateBody(infos);
 	})
-	div += "</div>"
 	printTicket(div);
 }
 
 function printTicket(text){
 	var a = window.open('', '', 'height=500, width=1000'); 
+	a.document.write(templateHead());  
 	a.document.write(text);  
-	a.document.close(); 
+	a.document.write(templateFoot());
 	setTimeout(function(){
-		a.print();
-	}, 100);
+		a.print();  
+		a.close(); 
+	}, 1000);
 }
 function toClipboard(str){
   const el = document.createElement('textarea');
